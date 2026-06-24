1d0536d1e60d38a751cbcae0842bdaa91392ae9b
Lucy <lucymwago9@gmail.com>
2026-06-24 15:41:02 +0300

    correct errors

--- lib/song.py
+++ lib/song.py
@@ -32,14 +32,14 @@ class Song:
 
     @classmethod
     def add_to_genre_count(cls, genre):
-        if genre not in cls.genre_count:
+        if genre in cls.genre_count:
             cls.genre_count[genre] += 1
         else:
             cls.genre_count[genre] = 1
     
     @classmethod
     def add_to_artist_count(cls, artist):
-        if artist not in cls.artist_count:
+        if artist in cls.artist_count:
             cls.artist_count[artist] += 1
         else:
             cls.artist_count[artist] = 1
